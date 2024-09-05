54946480d742975e0fadb46975fc824ebbd723ec
Pedro Bastos De Medeiros <pedromedeiros@PedroMedeiross-MacBook-Air.local>
2024-09-05 18:30:54 -0300

    Initial Commit

---
+++ src/routes/mod.rs
@@ -0,0 +1,21 @@
+use rocket::response::content::RawHtml;
+
+pub fn home() -> RawHtml<&'static str> {
+    RawHtml(include_str!("../templates/home.html"))
+}
+
+pub fn what_is_bitcoin() -> RawHtml<&'static str> {
+    RawHtml(include_str!("../templates/what_is_bitcoin.html"))
+}
+
+pub fn bitcoin_mining() -> RawHtml<&'static str> {
+    RawHtml(include_str!("../templates/bitcoin_mining.html"))
+}
+
+pub fn how_to_buy_bitcoin() -> RawHtml<&'static str> {
+    RawHtml(include_str!("../templates/how_to_buy_bitcoin.html"))
+}
+
+pub fn bitcoin_price_history() -> RawHtml<&'static str> {
+    RawHtml(include_str!("../templates/bitcoin_price_history.html"))
+}
