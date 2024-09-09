9727cf927c9695292126bfd4ff3ff77c5926efec
Pedro Bastos De Medeiros <pedromedeiros@PedroMedeiross-MacBook-Air.local>
2024-09-09 17:08:23 -0300

    new homepage layout, bitcoin price checker is on tests and new what is bitcoin page

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,17 @@ mod routes;
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> rocket::response::content::RawHtml<&'static str> {
-    routes::home()
-}
-
-#[get("/what-is-bitcoin")]
-fn what_is_bitcoin() -> rocket::response::content::RawHtml<&'static str> {
-    routes::what_is_bitcoin()
-}
-
-#[get("/bitcoin-mining")]
-fn bitcoin_mining() -> rocket::response::content::RawHtml<&'static str> {
-    routes::bitcoin_mining()
-}
-
-#[get("/how-to-buy-bitcoin")]
-fn how_to_buy_bitcoin() -> rocket::response::content::RawHtml<&'static str> {
-    routes::how_to_buy_bitcoin()
-}
-
-#[get("/bitcoin-price-history")]
-fn bitcoin_price_history() -> rocket::response::content::RawHtml<&'static str> {
-    routes::bitcoin_price_history()
-}
-
 use rocket::fs::FileServer;
-
-// ... (previous code remains the same)
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, what_is_bitcoin, bitcoin_mining, how_to_buy_bitcoin, bitcoin_price_history])
+        .mount("/", routes![
+            routes::home,
+            routes::what_is_bitcoin,
+            routes::bitcoin_mining,
+            routes::how_to_buy_bitcoin,
+            routes::bitcoin_price_history
+        ])
         .mount("/static", FileServer::from("static"))
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,20 +8,20 @@ pub fn home() -> RawHtml<&'static str> {
 
 #[get("/what-is-bitcoin")]
 pub fn what_is_bitcoin() -> RawHtml<&'static str> {
-    RawHtml(include_str!("../templates/what_is_bitcoin.html"))
+    RawHtml(include_str!("../../what_is_bitcoin.html"))
 }
 
 #[get("/bitcoin-mining")]
 pub fn bitcoin_mining() -> RawHtml<&'static str> {
-    RawHtml(include_str!("../templates/bitcoin_mining.html"))
+    RawHtml(include_str!("../../bitcoin_mining.html"))
 }
 
 #[get("/how-to-buy-bitcoin")]
 pub fn how_to_buy_bitcoin() -> RawHtml<&'static str> {
-    RawHtml(include_str!("../templates/how_to_buy_bitcoin.html"))
+    RawHtml(include_str!("../../how_to_buy_bitcoin.html"))
 }
 
 #[get("/bitcoin-price-history")]
 pub fn bitcoin_price_history() -> RawHtml<&'static str> {
-    RawHtml(include_str!("../templates/bitcoin_price_history.html"))
+    RawHtml(include_str!("../../bitcoin_price_history.html"))
 }
