ad544c74552c45189a6fe76701b107b218857227
Pedro Bastos De Medeiros <pedromedeiros@PedroMedeiross-MacBook-Air.local>
2024-09-09 16:39:35 -0300

    new homepage layout, bitcoin price checker is on tests and new what is bitcoin page

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,21 +1,27 @@
 use rocket::response::content::RawHtml;
+use rocket::get;
 
+#[get("/")]
 pub fn home() -> RawHtml<&'static str> {
     RawHtml(include_str!("../../index.html"))
 }
 
+#[get("/what-is-bitcoin")]
 pub fn what_is_bitcoin() -> RawHtml<&'static str> {
     RawHtml(include_str!("../templates/what_is_bitcoin.html"))
 }
 
+#[get("/bitcoin-mining")]
 pub fn bitcoin_mining() -> RawHtml<&'static str> {
     RawHtml(include_str!("../templates/bitcoin_mining.html"))
 }
 
+#[get("/how-to-buy-bitcoin")]
 pub fn how_to_buy_bitcoin() -> RawHtml<&'static str> {
     RawHtml(include_str!("../templates/how_to_buy_bitcoin.html"))
 }
 
+#[get("/bitcoin-price-history")]
 pub fn bitcoin_price_history() -> RawHtml<&'static str> {
     RawHtml(include_str!("../templates/bitcoin_price_history.html"))
 }
